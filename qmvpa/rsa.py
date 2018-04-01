2d0845d60667326ae77dfeb79e98a4cd6c14154d
QiHong Lu <lvqihong1992@gmail.com>
2018-04-01 16:33:17 -0400

    optimization + doc

--- qmvpa/rsa.py
+++ qmvpa/rsa.py
@@ -1,12 +1,20 @@
-""" representational similarity analysis
+"""Functions for Representational Similarity Analysis
 """
 
 import numpy as np
-from scipy.stats.stats import pearsonr
 
 
 def within_RSMs(Xs):
-    """ given a list of Xs, compute all within-subject RSM
+    """Compute all within-subject RSMs
+    Parameters
+    ----------
+    Xs: a list of 2d array in the form of (n_feature_dim x n_examples)
+        the activation matrices
+
+    Returns
+    -------
+    rsms
+        a list of representational similarity matrices
     """
     # compute RSM
     num_subjects = len(Xs)
@@ -15,62 +23,112 @@ def within_RSMs(Xs):
 
 
 def correlate_2RSMs(rsm1, rsm2):
-    """compute the correlation between 2 RSMs
+    """Compute the correlation between 2 RSMs
+    Parameters
+    ----------
+    rsm_i: a 2d array in the form of (n_examples x n_examples)
+        a representational similarity matrix
+
+    Returns
+    -------
+    r: float
+        linear_correlation(rsm1, rsm2)
     """
     # compute the linear correlation of 2 vectorized RSMs
-    r, _ = pearsonr(np.reshape(rsm1, (-1,)), np.reshape(rsm2, (-1,)))
+    r = np.corrcoef(np.reshape(rsm1, (-1,)), np.reshape(rsm2, (-1,)))[0, 1]
     return r
 
 
 def correlate_RSMs(rsms):
-    """ given a list of rsms, compute all pairwise corr
+    """Compute correlation between RSMs (2nd order correlations)
+    Parameters
+    ----------
+    rsms: a list of 2d array in the form of (n_examples x n_examples)
+        representational similarity matrces
+
+    Returns
+    -------
+    rsm_corrs: 2d array in the form of (num_rsms x num_rsms)
     """
     num_subjects = len(rsms)
-    corr_rsms = np.zeros((num_subjects, num_subjects))
+    rsm_corrs = np.zeros((num_subjects, num_subjects))
     for i in range(num_subjects):
-        for j in range(num_subjects):
-            corr_rsms[i, j] = correlate_2RSMs(
+        for j in np.arange(0, i+1, 1):
+            rsm_corrs[i, j] = correlate_2RSMs(
                 rsms[i], rsms[j])
-    return corr_rsms
+    # fillin the upper triangular part by symmetry
+    rsm_corrs = reflect_upper_triangular_part(rsm_corrs)
+    return rsm_corrs
 
 
 def inter_RSM(m1, m2):
-    """ compute the RSM for 2 activation matrices
-        input: mi (n_feature_dim x n_examples)
-        action: compute corr(col_i(m1), col_j(m2)) for all i and j
+    """Compute the RSM for 2 activation matrices
+    Parameters
+    ----------
+    mi: 2d array (n_feature_dim x n_examples)
+        a activation matrix
+
+    Returns
+    -------
+    intersubj_rsm
+        corr(col_i(m1), col_j(m2)), for all i and j
     """
     assert(np.shape(m1)[1] == np.shape(m2)[1])
     n_examples = np.shape(m1)[1]
     # compute the correlation matrix of hidden activity
-    corr_mat = np.zeros((n_examples, n_examples))
+    intersubj_rsm = np.zeros((n_examples, n_examples))
     for i in range(n_examples):
         for j in np.arange(0, i+1, 1):
-            corr_mat[i, j] = pearsonr(m1[:, i], m2[:, j])[0]
+            intersubj_rsm[i, j] = np.corrcoef(m1[:, i], m2[:, j])[0, 1]
             if all(m1[:, i] == 0) or all(m2[:, j] == 0):
-                corr_mat[i, j] = 0
+                intersubj_rsm[i, j] = 0
     # fillin the upper triangular part by symmetry
-    irows, icols = np.triu_indices(np.shape(corr_mat)[0], 1)
-    corr_mat[irows, icols] = corr_mat[icols, irows]
-    return corr_mat
+    intersubj_rsm = reflect_upper_triangular_part(intersubj_rsm)
+    return intersubj_rsm
 
 
 def inter_RSMs(matrix_list):
-    """ compute group-level intersubj_RSM
-        similar to the two subject case...
-        when comparing k subjects to 1 subject, average the k
+    """Compute intersubject representational similarity for a list of acts
+        when comparing (k-1) subjects to left-out subject, average the k
+
+    Parameters
+    ----------
+    matrix_list: a list of 2d array in the form of (n_feature_dim x n_examples)
+        the activation matrices
+
+    Returns
+    -------
+    intersubj_rsm
+        the average intersubject representational similarity matrix
     """
     matrix_array = np.array(matrix_list)
     len(matrix_list)
-    cross_subj_RSM = []
+    intersubj_rsms = []
     for loo_idx in range(len(matrix_list)):
         mean_Hs = np.mean(
             matrix_array[np.arange(len(matrix_list)) != loo_idx], axis=0)
-        cross_subj_RSM.append(inter_RSM(matrix_array[loo_idx], mean_Hs))
-    cross_subj_RSM = np.mean(cross_subj_RSM, axis=0)
-    return cross_subj_RSM
+        intersubj_rsms.append(inter_RSM(matrix_array[loo_idx], mean_Hs))
+    intersubj_rsm = np.mean(intersubj_rsms, axis=0)
+    return intersubj_rsm
 
 
-""" util functions """
+""" helper functions """
+
+
+def reflect_upper_triangular_part(matrix):
+    """Copy the lower triangular part to the upper triangular part
+        This speed up RSA computation
+    Parameters
+    ----------
+    matrix: a 2d array
+
+    Returns
+    -------
+    matrix with upper triangular part filled
+    """
+    irows, icols = np.triu_indices(np.shape(matrix)[0], 1)
+    matrix[irows, icols] = matrix[icols, irows]
+    return matrix
 
 
 def moving_window_avg(matrix, axis, win_size):
